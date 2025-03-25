dba48099ca98b5b93068ee04a69372328707b9ed
zarrukh <z.jurakulov@uzinfocom.uz>
2025-03-25 15:19:52 +0500

    some changes

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { Layout } from 'antd';
 export default function Footer() {
   return (
     <Layout.Footer style={{ textAlign: 'center', backgroundColor: '#fff' }}>
-      Xavfni tahlil qilish ©{new Date().getFullYear()} Created by{' '}
+      Xavfni tahlil qilish tizimi ©{new Date().getFullYear()} Created by{' '}
       <a href='https://uzinfocom.uz/' target='_blank' className='!text-blue-500'>
         UZINFOCOM
       </a>
