7686b86b3a8d68aa0c3160bcb9abb4fc2587a682
zarrukh <z.jurakulov@uzinfocom.uz>
2025-04-16 15:03:31 +0500

    added: file downloading

--- src/pages/AssessmentDetails/components/IndicatorsList.tsx
+++ src/pages/AssessmentDetails/components/IndicatorsList.tsx
@@ -4,7 +4,7 @@ import { JSX, useState } from 'react';
 import UpdateAssessments from './UpdateAssessments';
 import { DownloadOutlined, EditOutlined } from '@ant-design/icons';
 import { useProfile } from '@/shared/hooks/use-profile/use-profile';
-import { api, hasPermission } from '@/service';
+import { api, downloadFile, hasPermission } from '@/service';
 import { _RESPONSIBLE, _SUPER_ADMIN, _TERRITORIAL_RESPONSIBLE } from '@/service/const/roles';
 const { Column } = Table;
 
@@ -75,6 +75,23 @@ export default function IndicatorsList({ request }: { request: any }): JSX.Eleme
           title={t("Xavf darajasini baholash ko'rsatkichi")}
           dataIndex='indicator'
           render={(indicator) => indicator.name}
+        />
+        <Column
+          render={(item) => {
+            return (
+              <div className='flex  items-center gap-2'>
+                {item.file?.id && (
+                  <Button
+                    type='primary'
+                    size='large'
+                    title={t('Fayl yuklash')}
+                    icon={<DownloadOutlined />}
+                    onClick={() => downloadFile(item.file.id, item.file.name)}
+                  ></Button>
+                )}
+              </div>
+            );
+          }}
         />
         <Column align='center' title={t('Ball')} dataIndex='score' key='score' />
       </Table>
