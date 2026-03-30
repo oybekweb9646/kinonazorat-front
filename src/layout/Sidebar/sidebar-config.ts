56d9529d02e46bd4ceb52e9bd1317d9b0ddb78ed
oybekweb9646 <77285470+oybekweb9646@users.noreply.github.com>
2026-03-30 10:20:09 +0500

    Update sidebar-config.ts

--- src/layout/Sidebar/sidebar-config.ts
+++ src/layout/Sidebar/sidebar-config.ts
@@ -11,7 +11,7 @@ export const pathToKeyMap: { [key: string]: string[] } = {
   '/settings/indicators': ['7', '7.2'],
   '/settings/users': ['7', '7.3'],
   '/settings/organizations': ['7', '7.4'],
-  '/settings/aoka-organizations': ['7', '7.5'],
+  // '/settings/aoka-organizations': ['7', '7.5'],
   // '/settings/checklists': ['7', '7.5'],
   '/settings/normative-documents': ['7', '7.6'],
 };
